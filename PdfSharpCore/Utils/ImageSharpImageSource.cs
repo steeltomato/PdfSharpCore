84ec353b988473ddfdbe01f118d33a5b77828e78
Ken Hill <kenneth.hill@coxautoinc.com>
2023-11-02 15:48:02 -0400

    Update ImageSharp, take in breaking change and restrict to net6+

--- PdfSharpCore/Utils/ImageSharpImageSource.cs
+++ PdfSharpCore/Utils/ImageSharpImageSource.cs
@@ -21,22 +21,22 @@ namespace PdfSharpCore.Utils
 
         protected override IImageSource FromBinaryImpl(string name, Func<byte[]> imageSource, int? quality = 75)
         {
-            var image = Image.Load<TPixel>(imageSource.Invoke(), out IImageFormat imgFormat);
-            return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
+            var image = Image.Load<TPixel>(imageSource.Invoke());
+            return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, image.Metadata.DecodedImageFormat is PngFormat);
         }
 
         protected override IImageSource FromFileImpl(string path, int? quality = 75)
         {
-            var image = Image.Load<TPixel>(path, out IImageFormat imgFormat);
-            return new ImageSharpImageSourceImpl<TPixel>(path, image, (int) quality, imgFormat is PngFormat);
+            var image = Image.Load<TPixel>(path);
+            return new ImageSharpImageSourceImpl<TPixel>(path, image, (int) quality, image.Metadata.DecodedImageFormat is PngFormat);
         }
 
         protected override IImageSource FromStreamImpl(string name, Func<Stream> imageStream, int? quality = 75)
         {
             using (var stream = imageStream.Invoke())
             {
-                var image = Image.Load<TPixel>(stream, out IImageFormat imgFormat);
-                return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
+                var image = Image.Load<TPixel>(stream);
+                return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, image.Metadata.DecodedImageFormat is PngFormat);
             }
         }
 
